de19cbd44dac51ef1bcc55fea17b53e380d374d3
Sean Campbell <sean.campbell13@gmail.com>
2022-12-11 14:11:20 -0500

    js

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,6 +1,6 @@
 import type { ActionArgs, LinksFunction, MetaFunction } from "@remix-run/node"
 import { json } from "@remix-run/node"
-import { Link, useActionData, useSearchParams } from "@remix-run/react"
+import { Form, Link, useActionData, useSearchParams } from "@remix-run/react"
 
 import stylesUrl from "~/styles/login.css"
 import { db } from "~/utils/db.server"
@@ -118,7 +118,7 @@ export default function Login() {
     <div className="container">
       <div className="content" data-light="">
         <h1>Login</h1>
-        <form method="post">
+        <Form method="post">
           <input
             type="hidden"
             name="redirectTo"
@@ -202,7 +202,7 @@ export default function Login() {
           <button type="submit" className="button">
             Submit
           </button>
-        </form>
+        </Form>
       </div>
       <div className="links">
         <ul>
